ec1ab96a57087500e3941c0bfbbf688bac5879a1
Zhdzd <zahidabdullah18@gmail.com>
2022-01-07 11:08:19 +0800

    'commit'

---
+++ src/test/java/com/example/Day13Workshop/Day13WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day13Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
